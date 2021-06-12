454d97b665801f11acb6cc45d36ce763efb6024d
ShirAvneri <shiravneri@gmail.com>
2021-06-12 14:04:55 +0300

    Implemented a scoreboard and score changing collectable items
    * Merged all generators into Layout Generator.
    * Implemented a simple player scoreboard on the screen.
    * Generating coins and boxes, which add to the player's score.

--- Assets/Scripts/LayoutGenerator.cs
+++ Assets/Scripts/LayoutGenerator.cs
@@ -4,21 +4,39 @@ using UnityEngine;
 
 public class LayoutGenerator : MonoBehaviour
 {
-    public int roomsQuantity = 6;
-    public GameObject container;
-    public GameObject prefab;
-    public GameObject floor;
-    private float xPosMin, xPosMax, zPosMin, zPosMax; // Floor's bounds
-    // Start is called before the first frame update
+    // These numbers will later be based on difficulty
+    private int roomsQuantity = 6;
+    private int potionsQuantity = 20;
+    private int moneyQuantity = 50;
+    /* ---------- Necessary prefabs ---------- */
+    public GameObject roomPrefab;
+    public GameObject redPotionPrefab;
+    public GameObject bluePotionPrefab;
+    public GameObject greenPotionPrefab;
+    public GameObject coinPrefab;
+    public GameObject boxPrefab;
+    /* ---------- Necessary containers ---------- */
+    public GameObject layoutContainer;
+    public GameObject floorContainer;
+    /* ---------- Other necessary info ---------- */
+    private float xPosMin, xPosMax, zPosMin, zPosMax, yPos; // floor bounds
+    private enum potionTypes { BLUE = 1, GREEN = 2, RED = 3 };
+    private int potionTypesQuantity = 3;
+    private enum moneyTypes { COIN = 1, BOX = 2 };
+    private int moneyTypesQuantity = 2;
+
     void Start()
     {
-        
-        MeshRenderer floorRenderer = floor.GetComponent<MeshRenderer>();
+        MeshRenderer floorRenderer = floorContainer.GetComponent<MeshRenderer>();
+        // Calculating floor bounds +-10, for some extra spacing
         xPosMin = floorRenderer.bounds.min.x + 10;
         zPosMin = floorRenderer.bounds.min.z + 10;
         xPosMax = floorRenderer.bounds.max.x - 10;
         zPosMax = floorRenderer.bounds.max.z - 10;
+        yPos = floorRenderer.bounds.min.y;
         StartCoroutine(GenerateRooms());
+        StartCoroutine(GeneratePotions());
+        StartCoroutine(GenerateMoney());
     }
 
     IEnumerator GenerateRooms()
@@ -26,14 +44,77 @@ public class LayoutGenerator : MonoBehaviour
         int currentRoomsQuantity = 0;
         while (currentRoomsQuantity < roomsQuantity)
         {
-            float xRandomPos = Random.Range(xPosMin, xPosMax); // Based on current layout
-            float zRandomPos = Random.Range(zPosMin, zPosMax); // Based on current layout
-            GameObject generatedRoom = Instantiate(prefab, new Vector3(xRandomPos, 0, zRandomPos), Quaternion.identity);
-            generatedRoom.transform.SetParent(container.transform);
+            float xRandomPos = Random.Range(xPosMin, xPosMax); 
+            float zRandomPos = Random.Range(zPosMin, zPosMax);
+            GameObject generatedRoom = Instantiate(roomPrefab, new Vector3(xRandomPos, 0, zRandomPos), Quaternion.identity);
+            generatedRoom.transform.SetParent(layoutContainer.transform);
             Renderer renderer = generatedRoom.GetComponent<Renderer>();
             generatedRoom.AddComponent<MeshCollider>();
             yield return new WaitForSeconds(0f);
             currentRoomsQuantity++;
+        }
+    }
+
+    IEnumerator GeneratePotions()
+    {
+        int currentPotionsQuantity = 0;
+        while (currentPotionsQuantity < potionsQuantity)
+        {
+            float xRandomPos = Random.Range(xPosMin, xPosMax);
+            float zRandomPos = Random.Range(zPosMin, zPosMax); 
+            potionTypes potionType = (potionTypes)Random.Range(1, potionTypesQuantity + 1);
+            switch (potionType)
+            {
+                case potionTypes.BLUE:
+                    {
+                        GameObject generatedPotion = Instantiate(bluePotionPrefab, new Vector3(xRandomPos, yPos, zRandomPos), Quaternion.identity);
+                        generatedPotion.transform.parent = layoutContainer.transform;
+                        break;
+                    }
+                case potionTypes.RED:
+                    {
+                        GameObject generatedPotion = Instantiate(redPotionPrefab, new Vector3(xRandomPos, yPos, zRandomPos), Quaternion.identity);
+                        generatedPotion.transform.parent = layoutContainer.transform;
+                        break;
+                    }
+                case potionTypes.GREEN:
+                    {
+                        GameObject generatedPotion = Instantiate(greenPotionPrefab, new Vector3(xRandomPos, yPos, zRandomPos), Quaternion.identity);
+                        generatedPotion.transform.parent = layoutContainer.transform;
+                        break;
+                    }
+            }
+
+            yield return new WaitForSeconds(0f);
+            currentPotionsQuantity++;
+        }
+    }
+
+    IEnumerator GenerateMoney()
+    {
+        int currentMoneyQuantity = 0;
+        while (currentMoneyQuantity < moneyQuantity)
+        {
+            float xRandomPos = Random.Range(xPosMin, xPosMax); // Based on current layout
+            float zRandomPos = Random.Range(zPosMin, zPosMax); // Based on current layout
+            moneyTypes potionType = (moneyTypes)Random.Range(1, moneyTypesQuantity + 1);
+            switch (potionType)
+            {
+                case moneyTypes.COIN:
+                    {
+                        GameObject generatedMoney = Instantiate(coinPrefab, new Vector3(xRandomPos, yPos, zRandomPos), Quaternion.identity);
+                        generatedMoney.transform.parent = layoutContainer.transform;
+                        break;
+                    }
+                case moneyTypes.BOX:
+                    {
+                        GameObject generatedMoney = Instantiate(boxPrefab, new Vector3(xRandomPos, yPos, zRandomPos), Quaternion.identity);
+                        generatedMoney.transform.parent = layoutContainer.transform;
+                        break;
+                    }
+            }
+            yield return new WaitForSeconds(0f);
+            currentMoneyQuantity++;
         }
     }
 }
--- Assets/Scripts/PlayerStatsController.cs
+++ Assets/Scripts/PlayerStatsController.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class PlayerStatsController : MonoBehaviour
 {
+    /* Player score stats */
+    private ScoreController score;
+    private int coinScore = 5;
+    private int boxMaxScore = 25;
+    private int patientKill = 25;
+    private int nurseKill = 50;
+    private int doctorKill = 100;
+    private int guardKill = 200;
     /* Player health stats */
     public HealthBarController healthBar;
     private float maxHealth = 100;
     private float healingPotion = 20;
     private float punchDamage = 5;
     /* Player time stats */
-    public TimerController timer;
+    private TimerController timer;
     private float levelTime = 60 * 5 + 30;
     private float timePotion = 30;
-
     /* Player teleportation stats */
-    public int initialTeleportation = 10;
+    private int initialTeleportation = 10;
     private int currentTeleportation;
 
     void Start()
     {
+        score = GetComponent<ScoreController>();
+        score.SetCoinScore(coinScore);
+        score.SetBoxMaxScore(boxMaxScore);
         healthBar.SetMaxHealth(maxHealth);
         healthBar.SetPotionHeal(healingPotion);
         healthBar.SetPunchDamage(punchDamage);
@@ -44,6 +54,16 @@ public class PlayerStatsController : MonoBehaviour
     {
         currentTeleportation += 5;
         Debug.Log(currentTeleportation);
+    }
+
+    public void PickCoin()
+    {
+        score.Coin();
+    }
+
+    public void PickBox()
+    {
+        score.Box();
     }
 
     public void TakePunchDamage()
